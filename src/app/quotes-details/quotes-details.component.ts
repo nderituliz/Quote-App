19c31db8251327f04a00e6e37333146c0ce228b8
Elizabeth Nderitu <nderituliz@gmail.com>
2020-11-08 16:12:33 +0300

    Add input decorator for the quotes objects

---
+++ src/app/quotes-details/quotes-details.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
+import {Quotes} from '../quotes';
+
+@Component({
+  selector: 'app-quotes-details',
+  templateUrl: './quotes-details.component.html',
+  styleUrls: ['./quotes-details.component.css']
+})
+export class QuotesDetailsComponent implements OnInit {
+  @Input() quotes : Quotes;
+ 
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
