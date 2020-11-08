0cb45819965912dd8f4bc3d6e431cf1a66d7b8a7
Elizabeth Nderitu <nderituliz@gmail.com>
2020-11-08 22:09:02 +0300

    Push to github

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
+import { InputFormComponent } from './input-form/input-form.component';
+import { VoteComponent } from './vote/vote.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuotesComponent,
+    QuotesDetailsComponent,
+    InputFormComponent,
+    VoteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
